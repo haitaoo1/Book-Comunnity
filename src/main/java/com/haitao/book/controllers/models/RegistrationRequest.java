a8580972b94da775e1ecbe3a7ff6d1abbee4e98d
Haitao Wu <haitaowu14@gmail.com>
2024-07-06 13:10:24 +0200

    fixing bug

--- src/main/java/com/haitao/book/controllers/models/RegistrationRequest.java
+++ src/main/java/com/haitao/book/controllers/models/RegistrationRequest.java
@@ -1,5 +1,6 @@
 package com.haitao.book.controllers.models;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
@@ -20,6 +21,7 @@ public class RegistrationRequest {
     private String lastname;
     @NotEmpty(message = "email name is required")
     @NotBlank(message = "email name is required")
+    @Email(message = "Email format is incorrect")
     private String email;
     @NotEmpty(message = "username name is required")
     @NotBlank(message = "username name is required")
