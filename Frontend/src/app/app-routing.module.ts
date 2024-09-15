2c8755cd63afe0ae2073a485a59ca1147a3dc84b
Haitao Wu <haitaowu14@gmail.com>
2024-09-15 22:54:57 +0200

    BookComunnity

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import {ActivateAccountComponent} from "./pages/activate-account/activate-accoun
 const routes: Routes = [
   {path:'login', component: LoginComponent},
   {path: 'register',component: RegisterComponent},
-  {path: 'activate-account',component: ActivateAccountComponent}
+  {path: 'activate-account',component: ActivateAccountComponent},
+  {path: 'books', loadChildren: () => import('./module/book/book.module').then(m => m.BookModule)}
 ];
 
 @NgModule({
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {LoginComponent} from "./pages/login/login.component";
 import {FormsModule} from "@angular/forms";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from "@angular/common/http";
 import { RegisterComponent } from './pages/register/register.component';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 import {CodeInputModule} from "angular-code-input";
+import {HttpTokenInterceptor} from "./services/interceptor/http-token.interceptor";
 
 @NgModule({
   declarations: [
@@ -27,7 +28,12 @@ import {CodeInputModule} from "angular-code-input";
 
     ],
   providers: [
-    HttpClient
+    HttpClient,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass:HttpTokenInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
